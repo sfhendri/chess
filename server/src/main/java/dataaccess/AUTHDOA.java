e9a7d999c1b72b38b8a820cb9c38cf2b527f913f
sfhendri02 <seth@neuronovaco.com>
2026-03-02 09:16:57 -0700

    Create Server for testing

---
+++ server/src/main/java/dataaccess/AUTHDOA.java
@@ -0,0 +1,15 @@
+//package dataaccess;
+//
+//import model.AuthData;
+//
+//public interface AuthDAO {
+//
+//    void addAuth(AuthData authData);
+//
+//    void deleteAuth(String authToken);
+//
+//    AuthData getAuth(String authToken) throws DataAccessException;
+//
+//    void clear();
+//
+//}
