4b342cc643b86b6c74c7a679996fc4171eeced57
sfhendri02 <seth@neuronovaco.com>
2026-03-11 12:16:54 -0600

    Added logic to CodedException

--- server/src/main/java/service/CodedException.java
+++ server/src/main/java/service/CodedException.java
@@ -8,6 +8,22 @@ public class CodedException extends Exception {
         this.statusCode = statusCode;
     }
 
+    public CodedException(int statusCode, String message, Exception cause) {
+        super(message, cause);
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public String toString() {
+        var cause = getCause();
+        if (cause != null) {
+            return String.format("%s: %s", getMessage(), cause.getMessage());
+
+        }
+
+        return super.toString();
+    }
+
     public int statusCode() {
         return statusCode;
     }
