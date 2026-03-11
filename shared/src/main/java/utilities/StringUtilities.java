c0b77b6e955acdb8c6692db8a121ef12147eead5
sfhendri02 <seth@neuronovaco.com>
2026-03-11 12:30:02 -0600

    Adding more logic

--- shared/src/main/java/utilities/StringUtilities.java
+++ shared/src/main/java/utilities/StringUtilities.java
@@ -1,5 +1,7 @@
 package utilities;
 
+import java.util.Random;
+
 public final class StringUtilities {
 
     private StringUtilities() {
@@ -8,5 +10,13 @@ public final class StringUtilities {
 
     public static boolean isNullOrEmpty(String s) {
         return s == null || s.isEmpty();
+    }
+
+
+    private static final Random RANDOM = new Random();
+
+    public static String randomString() {
+        long randomNumber = Math.abs(RANDOM.nextLong());
+        return Long.toString(randomNumber, 36);
     }
 }
