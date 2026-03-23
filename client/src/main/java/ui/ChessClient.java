9eda5fa772a9ba9e5c9b79603f52de89121c6bf6
sfhendri02 <seth@neuronovaco.com>
2026-03-23 10:15:27 -0600

    Creating ChessClient Class

---
+++ client/src/main/java/ui/ChessClient.java
@@ -0,0 +1,15 @@
+package ui;
+
+import chess.ChessGame;
+import chess.ChessPosition;
+import model.AuthData;
+import model.GameData;
+import utilities.StringUtilities;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+import static ui.EscapeSequences.*;
+
+public class ChessClient {
+}
