0a78d808a610a13dd462ebb0569448937ad2c4e8
sfhendri02 <seth@neuronovaco.com>
2026-03-23 10:39:54 -0600

    Added Service class for server logic (Not implemented Yet)

---
+++ client/src/main/java/service/ServiceFacade.java
@@ -0,0 +1,15 @@
+package service;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+
+import java.net.URI;
+import java.net.http.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServiceFacade {
+
+}
