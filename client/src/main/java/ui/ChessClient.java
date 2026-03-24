6faf1872ae031567ee369b40cd1d5c1e1519502c
sfhendri02 <seth@neuronovaco.com>
2026-03-24 11:59:45 -0600

    Beginning logic for ChessClient

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -4,6 +4,7 @@ import chess.ChessGame;
 import chess.ChessPosition;
 import model.AuthData;
 import model.GameData;
+import service.ServerFacade;
 import utilities.StringUtilities;
 
 import java.lang.reflect.InvocationTargetException;
@@ -12,5 +13,15 @@ import java.util.*;
 import static ui.EscapeSequences.*;
 
 public class ChessClient {
+    final private ServerFacade server;
+    private State userState = State.LOGGED_OUT;
+    private String authToken;
+    private GameData gameData;
+    private List<GameData> games = new ArrayList<>();
+
+    public ChessClient() throws Exception {
+        server = new ServerFacade("http://localhost:8080");
+    }
+
 
 }
