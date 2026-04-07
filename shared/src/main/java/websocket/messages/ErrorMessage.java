b2cd30c013202dd4b729b77d28e390a4e7d8644f
sfhendri02 <seth@neuronovaco.com>
2026-04-07 14:35:50 -0600

    Added ErrorMessage

--- shared/src/main/java/websocket/messages/ErrorMessage.java
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -1,4 +1,21 @@
 package websocket.messages;
 
-public class ErrorMessage {
+import com.google.gson.Gson;
+
+public class ErrorMessage extends ServerMessage {
+    private String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
 }
