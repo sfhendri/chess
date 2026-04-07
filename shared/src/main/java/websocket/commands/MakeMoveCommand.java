204d116ea1cf4b3ec4d47f9d81dc3d8853b1e024
sfhendri02 <seth@neuronovaco.com>
2026-04-07 14:34:29 -0600

    Created MakeMoveCommand

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,4 +1,19 @@
 package websocket.commands;
 
-public class MakeMoveCommand {
+import chess.ChessMove;
+import com.google.gson.Gson;
+
+import java.util.Objects;
+
+public class MakeMoveCommand extends UserGameCommand {
+    private final ChessMove move;
+
+    public MakeMoveCommand(String authToken, int gameID, ChessMove move) {
+        super(CommandType.MAKE_MOVE, authToken, gameID);
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
 }
